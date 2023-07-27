ca3cb11b14850647e1299004947a99ae0b94e6cd
worshipper <shakibprof2001@gmail.com>
2023-07-27 21:33:36 +0600

    test

---
+++ Week 07/Day 07/Budget_of_Technex.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long r,icm,d;
+	    cin>>r;
+	    r=r*1000;
+	    icm=r/2;
+	    r=r/2;
+	    d=r/5;
+	    cout<<d<<endl;
+	}
+	return 0;
+}
