5d67741aa249ca3cd3717fe40146f86ac85eadd4
worshipper <shakibprof2001@gmail.com>
2023-08-06 20:09:08 +0600

    test

---
+++ Week 09/Day 03/1722A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        sort(s.begin(),s.end());
+        string name = "Timur";
+        sort(name.begin(),name.end());
+        if(s == name)
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+
+    return 0;
+}
