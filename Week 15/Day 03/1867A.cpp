6f3d2cab03adbf0228d4b2a3113b449d63bb7baa
worshipper <shakibprof2001@gmail.com>
2023-09-17 22:55:25 +0600

    test

---
+++ Week 15/Day 03/1867A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<pair<int,int>>a;
+        for(int i=0;i<n;i++)
+        {
+            int x;
+            cin>>x;
+            a.push_back({x,i});
+        }
+        sort(a.begin(),a.end());
+        int x = n;
+        for(int i=0;i<n;i++)
+        {
+            a[i].first = x;
+            x--;
+        }
+        int ans[n];
+        for(int i=0;i<n;i++)
+        {
+            ans[a[i].second] = a[i].first;
+        }
+        for(int i=0;i<n;i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+
+    return 0;
+}
---
+++ Week 15/Day 03/1867B.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int pair = 0, cnt = 0;
+        int l  = 0,r = n-1;
+        while(l<r)
+        {
+            if(s[l] == s[r])
+                pair+=2;
+            else
+                cnt++;
+            l++,r--;
+        }
+        string ans = "";
+        for(int i=0;i<=n;i++)
+        {
+            int total = i;
+            total -= cnt;
+            if(total<0)
+            {
+                ans.push_back('0');
+                continue;
+            }
+
+            total = max((total%2),total-pair);
+            total = max(0,total-(n%2));
+
+            if(total == 0)
+                ans.push_back('1');
+            else
+                ans.push_back('0');
+        }
+        cout<<ans<<endl;
+    }
+    
+
+    return 0;
+}
